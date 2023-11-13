827a8b538ee88224b5e434fd3254fb4978eb8899
Sanjana-Boligorla <130239451+Sanjana-Boligorla@users.noreply.github.com>
2023-11-13 07:14:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Index from './components/Index';
+import Info from './components/Info';
+import CityDateModal from './components/citydate'
+// index.js or App.js
+//import { library } from '@fortawesome/fontawesome-svg-core';
+//import { faInstagram, faFacebook, faYoutube, faTwitter, faAppStore, faGooglePlay, faCcPaypal, faGooglePay, faApplePay, faAmazonPay, faPaypal } from '@fortawesome/free-brands-svg-icons';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+//library.add(faInstagram, faFacebook, faYoutube, faTwitter, faAppStore, faGooglePlay, faCcPaypal, faGooglePay, faApplePay, faAmazonPay, faPaypal);
+
+// The rest of your application code
 
 function App() {
   return (
@@ -13,6 +23,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/info" element={<Info />} />
           <Route path="/" element={<Index />} />
         </Routes>
       </div>
