9aedf0404c6933ccfb59792829918df7fea3b3a8
sreevidya_1110 <sreevidya111003@gmail.com>
2023-11-15 17:50:02 +0530

    Integrated backend connection for signup and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,64 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './styles.css'; // Assuming you have a styles.css file for your styles
-import logo from "./logo.png"
+import logo from "./logo.png";
+import Axios from "axios";
 const Login = () => {
+  const[data,setData]=useState({
+    email:"",
+    password:""
+  });
+  const handleChange=(e)=>{
+      const value=e.target.value;
+      setData({
+          ...data,
+          [e.target.name]:value
+      });
+  };
+  const handleSubmit=(e)=>{
+      e.preventDefault();
+      const userData={
+          email:data.email,
+          password:data.password
+      };
+      Axios.post("http://localhost:4000/userRoute/login",userData).then((response)=>{
+          console.log("This is the response");
+          console.log(response.data.message);
+          if(response.data.status==200){
+              document.getElementById("userdoesnotexist").innerHTML=response.data.message;
+              document.getElementById("userexists").innerHTML="";
+          }
+          else{
+              document.getElementById("userexists").innerHTML=response.data.message;
+              document.getElementById("userdoesnotexist").innerHTML="";
+          }
+      });
+  };
+
   return (
     <div className="container1">
       <img src={logo} alt="Movie Ticket Booking Logo" className="logo" />
       <div className="login-box">
         <h2>Welcome to World of Cinema</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" required />
+            <label htmlFor="email">Email</label>
+            <input value={data.email} onChange={handleChange} type="text" name="email" required />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input value={data.password} onChange={handleChange} type="password" name="password" required />
+          </div>
+          <div>
+            <p id="userdoesnotexist" className='text-danger'></p>
+            <p id="userexists" className='text-success'></p>
           </div>
           <button type="submit">Login</button>
           <br />
-          <label>
-            <input type="checkbox" id="remember-me" name="remember-me" />
-            Remember me
-          </label>
+          
         </form>
-        <div className="forgot-password">
-          <button>Forgot Password?</button>
-        </div>
+        
         <div className="text-center">
           <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
           {/* The Link component is used to navigate to the /signup route */}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,26 +1,76 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import './styles.css'; // Assuming you have a styles.css file for your styles
 import { Link } from 'react-router-dom';
-import logo from "./logo.png"
+import logo from "./logo.png";
+import Axios from "axios";
+
 const SignUp = () => {
+    const[data,setData]=useState({
+        name:"",
+        email:"",
+        password:"",
+        cpassword:""
+    });
+    const handleChange=(e)=>{
+        const value=e.target.value;
+        setData({
+            ...data,
+            [e.target.name]:value
+        });
+    };
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const userData={
+            name:data.name,
+            email:data.email,
+            password:data.password,
+            cpassword:data.cpassword
+        };
+        Axios.post("http://localhost:4000/userRoute/register",userData).then((response)=>{
+            console.log("This is the response");
+            console.log(response.data.message);
+            if(response.data.status==200){
+                document.getElementById("emailexists").innerHTML=response.data.message;
+                document.getElementById("userregistered").innerHTML="";
+            }
+            else{
+                document.getElementById("userregistered").innerHTML=response.data.message;
+                document.getElementById("emailexists").innerHTML="";
+            }
+        });
+    };
+    
+    
+    
+    
     return (
         
         <div className="container1">
             <img src={logo} alt="Movie Ticket Booking Logo" className="logo" />
             <div className="signup-box">
                 <h2>Create an Account</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-group">
-                        <label htmlFor="signup-username">Username</label>
-                        <input type="text" id="signup-username" name="signup-username" required />
+                        <label htmlFor="name">Name</label>
+                        <input value={data.name} onChange={handleChange} type="text" name="name" required />
                     </div>
                     <div className="input-group">
-                        <label htmlFor="signup-email">Email</label>
-                        <input type="email" id="signup-email" name="signup-email" required />
+                        <label htmlFor="email">Email</label>
+                        <input value={data.email} onChange={handleChange} type="email" name="email" required />
+                    </div>
+                    
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input value={data.password} onChange={handleChange} type="password" name="password" required />
                     </div>
                     <div className="input-group">
-                        <label htmlFor="signup-password">Password</label>
-                        <input type="password" id="signup-password" name="signup-password" required />
+                        <label htmlFor="cpassword">Confirm Password</label>
+                        <input value={data.cpassword} onChange={handleChange} type="password" name="cpassword" required />
+                    </div>
+                    <div>
+                        <p  id="emailexists" className="text-danger"></p>
+                        <p id="userregistered" className='text-success'></p>
                     </div>
                     <button type="submit">Sign Up</button>
                 </form>
