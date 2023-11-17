ecbf24cd56f019f72b3749507ee8df86388dbdd6
Sanjana-Boligorla <130239451+Sanjana-Boligorla@users.noreply.github.com>
2023-11-17 15:20:36 +0530

    Update addMovie.js

--- src/components/adminPages/addMovie.js
+++ src/components/adminPages/addMovie.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../style1.css';
+import AdminHeader from './adminHeader';
+import Footer from '../footer';
 const AddMovie = () => {
     const [formData, setFormData] = useState({
         title: "",
@@ -116,51 +120,90 @@ const AddMovie = () => {
     };
 
     return (
-        <div>
-            <h1>Add A Movie</h1>
+        <div class="bg-black">
+            <AdminHeader />
+            <div class="container my-3">
+            <h1 class="mov m-3">Add A Movie</h1>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="title"> Title of the Movie : </label>
-                <input type='text' id='title' name='title' value={formData.title} onChange={handleInputChange} required /><br /><br />
-                <label htmlFor="moviePoster"> Movie Poster : </label>
-                <input type='file' accpet="image/*" id='moviePoster' name='moviePoster' onChange={handleImageChange} required /><br /><br />
-                <label htmlFor="releaseDate"> Release Date : </label>
-                <input type='date' id='releaseDate' name='releaseDate' value={formData.releaseDate} onChange={handleInputChange} required /><br /><br />
-                <label htmlFor='description'> Description of Movie : </label>
-                <textarea id='description' name='description' value={formData.description} onChange={handleInputChange} /><br /><br />
-                <label htmlFor='duration'> Duration : </label>
-                <input type='time' id='duration' name='duration' value={formData.duration} onChange={handleInputChange} /><br /><br />
-
-                <label htmlFor='genre'> Genre(s) (comma-separated) : </label>
-                <input type='text' id='genre' name='genre' value={formData.genre} onChange={handleMultipleChange} /><br /><br />
-
-                <label htmlFor='language'> Language(s) (comma-separated) : </label>
-                <input type='text' id='language' name='language' value={formData.language} onChange={handleMultipleChange} /><br /><br />
-
+                <table >
+                <tr>
+                    <th class="text-white p-3" >
+                <label htmlFor="title"> Title of the Movie : </label></th>
+                <td ><input type='text' id='title' name='title' value={formData.title} onChange={handleInputChange} required /></td>
+                </tr>
+                <tr >
+                <th class="text-white p-3"><label htmlFor="moviePoster"> Movie Poster : </label></th>
+                <td><input type='file' accept="image/*" id='moviePoster' name='moviePoster' onChange={handleImageChange} required />
+                </td>
+                </tr>
+                <tr><th class="text-white p-3">
+                <label htmlFor="releaseDate"> Release Date : </label></th>
+                <td><input type='date' id='releaseDate' name='releaseDate' value={formData.releaseDate} onChange={handleInputChange} required />
+                </td></tr>
+                <tr>
+                <th class="text-white p-3"><label htmlFor='description'> Description of Movie : </label></th>
+                <td><textarea id='description' name='description' value={formData.description} onChange={handleInputChange} />
+                </td></tr>
+                <tr>
+                 <th class="text-white p-3">  <label htmlFor='duration'> Duration : </label></th>
+                <td><input type='time' id='duration' name='duration' value={formData.duration} onChange={handleInputChange} />
+                </td></tr>
+                <tr>
+                    <th class="text-white p-3">
+                <label htmlFor='genre'> Genre(s) (comma-separated) : </label></th>
+                <td><input type='text' id='genre' name='genre' value={formData.genre} onChange={handleMultipleChange} />
+                </td></tr>
+                <tr>
+                    <th class="text-white p-3">
+                <label htmlFor='language'> Language(s) (comma-separated) : </label></th>
+                <td>
+                <input type='text' id='language' name='language' value={formData.language} onChange={handleMultipleChange} />
+            </td></tr>
+            <tr>
+                <th class="text-white p-3">
                 <label htmlFor='cast'> Cast (comma-separated) : </label>
-                <input type='text' id='cast' name='cast' value={formData.cast} onChange={handleMultipleChange} /><br /><br />
-
+            </th>
+            <td>
+                <input type='text' id='cast' name='cast' value={formData.cast} onChange={handleMultipleChange} />
+            </td></tr>
                 {formData.cast.map((member, index) => (
                     <div key={index}>
-                        <label htmlFor={`castImages${index}`}> Image for {member} : </label>
-                        <input type='file' accept='image/*' id={`castImages${index}`} name='castImages' onChange={(event) => handleMultipleImages(event)} /><br /><br />
+                        <tr>
+                        <th class="text-white p-3"><label htmlFor={`castImages${index}`}> Image for {member} : </label>
+                        </th>
+                        <td>
+                            <input type='file' accept='image/*' id={`castImages${index}`} name='castImages' onChange={(event) => handleMultipleImages(event)} />
+                        </td>
+                        </tr>
                     </div>
                 ))}
-
-                <label htmlFor='director'> Director(s) (comma-separated) : </label>
-                <input type='text' id='director' name='director' value={formData.director} onChange={handleMultipleChange} /><br /><br />
-
+                    <tr>
+                        <th class="text-white p-3">
+                <label htmlFor='director'> Director(s) (comma-separated) : </label></th>
+                <td>
+                <input type='text' id='director' name='director' value={formData.director} onChange={handleMultipleChange} />
+            </td>
+            </tr>
                 {formData.director.map((member, index) => (
                     <div key={index}>
-                        <label htmlFor={`directorImages${index}`}> Image for {member} : </label>
-                        <input type='file' accept='image/*' id={`directorImages${index}`} name='directorImages' onChange={(event) => handleMultipleImages(event)} /><br /><br />
+                        <tr>
+                            <th class="text-white p-3">
+                        <label htmlFor={`directorImages${index}`}> Image for {member} : </label></th>
+                        <td>
+                        <input type='file' accept='image/*' id={`directorImages${index}`} name='directorImages' onChange={(event) => handleMultipleImages(event)} />
+                    </td>
+                    </tr>
                     </div>
                 ))}
-
-                <button type='submit'> Add Movie </button>
+                </table>
+                <button type='submit' class="add rounded m-3"><b> Add Movie</b> </button>
             </form>
             {/* <input type="file" accept="image/*" onChange={handleImageChange} />
             <button onClick={handleImageUpload}>Upload Image</button> */}
         </div>
+        <Footer />
+        </div>
+
     );
 };
 
