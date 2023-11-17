9f15519560f8c18517224c48442e3cb1b479d10c
Sanjana-Boligorla <130239451+Sanjana-Boligorla@users.noreply.github.com>
2023-11-17 10:09:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import MoviePage from './components/moviespage';
 import About from './components/about';
 import Contact from './components/contact';
 import Ratings from './components/ratings';
-import AddMovie from ./components/adminPages/addMovie;
+import AddMovie from './components/adminPages/addMovie';
 // index.js or App.js
 //import { library } from '@fortawesome/fontawesome-svg-core';
 //import { faInstagram, faFacebook, faYoutube, faTwitter, faAppStore, faGooglePlay, faCcPaypal, faGooglePay, faApplePay, faAmazonPay, faPaypal } from '@fortawesome/free-brands-svg-icons';
