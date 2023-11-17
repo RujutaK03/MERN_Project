2b73baeb85d578726f70c7cc9624cb9ca83dd7d4
Sanjana-Boligorla <130239451+Sanjana-Boligorla@users.noreply.github.com>
2023-11-17 15:23:00 +0530

    Update displayTheatre.js

--- src/components/adminPages/displayTheatre.js
+++ src/components/adminPages/displayTheatre.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../style1.css';
+import AdminHeader from './adminHeader';
+import Footer from '../footer';
 
 const DisplayTheatre = () => {
     const [theatres, setTheatres] = useState([]);
@@ -11,18 +16,22 @@ const DisplayTheatre = () => {
     }, []);
 
     return (
-        <div>
-            <h1> All Theatres </h1>
+        <div class="bg-black">
+            <AdminHeader />
+            <div class="container my-3">
+            <h1 class="mov"> All Theatres </h1>
             {theatres.map((theatre, index) => (
                 <div key={index}>
                     <img src={`http://localhost:8080/get-theatre/${theatre.image.filename}`}
                         alt={theatre.name}
                         style={{ 'maxWidth': '300px', 'maxHeight': '300px' }} />
 
-                    <h5> {theatre.name} </h5>
-                    <h5> Location : {theatre.location} </h5>
+                    <h5 class="text-white"> {theatre.name} </h5>
+                    <h5 class="text-white"> Location : {theatre.location} </h5>
                 </div>
             ))}
+            </div>
+            <Footer />
         </div>
     )
 }
