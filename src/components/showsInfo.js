25d4751a97c656906fae12d7e1b5ffe663daf85f
Sanjana-Boligorla <130239451+Sanjana-Boligorla@users.noreply.github.com>
2023-11-21 21:37:18 +0530

    Update showsInfo.js

--- src/components/showsInfo.js
+++ src/components/showsInfo.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './style1.css';
 import Header from './header';
 import Footer from './footer';
+import { Link } from 'react-router-dom';
 
 const ShowInfo = () => {
     const [shows, setShows] = useState([{
@@ -19,7 +20,7 @@ const ShowInfo = () => {
     const date = searchParams.get("date")
 
     useEffect(() => {
-        axios.get("https://mern-project-deployment-1.onrender.com/get-shows", {
+        axios.get("http://localhost:8080/get-shows", {
             params: { movieId: movieId, theatreId: theatreId }
         })
             .then((res) => {
@@ -34,9 +35,84 @@ const ShowInfo = () => {
                 console.log(error); setData(false);
             })
     }, [movieId, theatreId]);
-
+    document.addEventListener('DOMContentLoaded', function () {
+        const container1 = document.querySelector('.container1');
+        const container2 = document.querySelector('.container2');
+        const totalAmountElement = document.getElementById('totalAmount');
+        let totalAmount = 0;
+  
+        // Create 10 seats dynamically
+        for (let i = 1; i <= 25; i++) {
+          const seatButton = document.createElement('button');
+          seatButton.textContent = i;
+          
+  
+          // Event listener for seat button click
+          seatButton.addEventListener('click', function () {
+            // Toggle seat selection by adding/removing 'selected' class
+            this.classList.toggle('selected');
+  
+            // Update total amount based on selected seats
+            totalAmount = calculateTotal();
+            totalAmountElement.textContent = totalAmount;
+          });
+  
+          // Event listener for hover
+          seatButton.addEventListener('mouseenter', function () {
+            // Change color to dark blue on hover
+            this.style.backgroundColor = 'darkblue';
+          });
+  
+          // Event listener for mouse leave (to reset the color)
+          seatButton.addEventListener('mouseleave', function () {
+            // Reset color to the original state on mouse leave
+            this.style.backgroundColor = this.classList.contains('selected') ? 'green' : 'blue';
+          });
+  
+          // Append each seat button to the container
+          container1.appendChild(seatButton);
+        }
+        for (let i = 1; i <= 25; i++) {
+          const seatButton = document.createElement('button');
+          seatButton.textContent = i;
+          
+          // Event listener for seat button click
+          seatButton.addEventListener('click', function () {
+            // Toggle seat selection by adding/removing 'selected' class
+            this.classList.toggle('selected');
+  
+            // Update total amount based on selected seats
+            totalAmount = calculateTotal();
+            totalAmountElement.textContent = totalAmount;
+          });
+  
+          // Event listener for hover
+          seatButton.addEventListener('mouseenter', function () {
+            // Change color to dark blue on hover
+            this.style.backgroundColor = 'darkblue';
+          });
+  
+          // Event listener for mouse leave (to reset the color)
+          seatButton.addEventListener('mouseleave', function () {
+            // Reset color to the original state on mouse leave
+            this.style.backgroundColor = this.classList.contains('selected') ? 'green' : 'blue';
+          });
+  
+          // Append each seat button to the container
+          container2.appendChild(seatButton);
+        }
+  
+  
+        // Function to calculate the total amount based on selected seats
+        function calculateTotal() {
+          const selectedSeats = document.querySelectorAll('.selected');
+          const seatPrice = 200;
+          return selectedSeats.length * seatPrice;
+        }
+      });
+  
     return (
-       <div class="bg-black">
+        <div class="bg-black">
             <Header />
             <h1 class="mov container my-3">Available Shows</h1>
             {data ? (
@@ -59,7 +135,6 @@ const ShowInfo = () => {
             ) : (<p class="text-white m-2 h2 text-center"> No Shows Available </p>)}
                         <Footer />
         </div>
-
     )
 
 }
