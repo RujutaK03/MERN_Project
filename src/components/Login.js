f3aea7080e79aa1c20808034c5335b3402187557
abhimanyu90 <96384184+abhimanyu90@users.noreply.github.com>
2023-11-22 12:11:00 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -64,11 +64,11 @@ const Login = () => {
             <p id="userdoesnotexist" className='text-danger'></p>
             <p id="userexists" className='text-success'></p>
           </div>
-          <button type="submit">Login</button>
+          <button type="submit" onClick={handleRedirect}>Login</button>
           <br />
           
         </form>
-      <Link to="/" id="redirectLink" style={{ display: 'none' }}></Link>
+      
         
         <div className="text-center">
           <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
