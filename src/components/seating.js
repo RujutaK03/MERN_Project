e78b095d4cbd22f6265f67cf488faa8c6994bc4f
Sanjana-Boligorla <130239451+Sanjana-Boligorla@users.noreply.github.com>
2023-11-23 07:50:24 +0530

    Update seating.js

--- src/components/seating.js
+++ src/components/seating.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './style1.css';
 import Header from './header';
 import Footer from './footer';
+
 const MovieSeatSelection = () => {
   const [totalAmount, setTotalAmount] = useState(0);
 
@@ -21,13 +22,13 @@ const MovieSeatSelection = () => {
     setTotalAmount(calculateTotal());
   };
 
-  const createSeatButtons = (containerId) => {
+  const createSeatButtons = (containerId, start, end) => {
     const seatButtons = [];
-    for (let i = 1; i <= 25; i++) {
+    for (let i = start; i <= end; i++) {
       seatButtons.push(
         <button
           key={i}
-          class="seat rounded border"
+          className="seat rounded border"
           onClick={handleSeatClick}
           onMouseEnter={(e) => (e.target.style.backgroundColor = 'darkblue')}
           onMouseLeave={(e) =>
@@ -42,18 +43,18 @@ const MovieSeatSelection = () => {
   };
 
   return (
-    <div class="bg-black">
-        <Header />
-      <h1 class="mov text-center my-3">Movie Seat Selection</h1>
-      <div class="screen bg-dark text-white w-25 text-center mx-auto my-3 border">All Eyes This Way Please</div>
-      <div class="d-flex justify-content-center">
-        <div className="container3">{createSeatButtons('container3')}</div>
-        <div className="container4">{createSeatButtons('container4')}</div>
+    <div className="bg-black">
+      <Header />
+      <h1 className="mov text-center my-3">Movie Seat Selection</h1>
+      <div className="screen bg-dark text-white w-25 text-center mx-auto my-3 border">All Eyes This Way Please</div>
+      <div className="d-flex justify-content-center">
+        <div className="container3">{createSeatButtons('container3', 1, 25)}</div>
+        <div className="container4">{createSeatButtons('container4', 26, 50)}</div>
       </div>
       <div className="total">
-        <h3 class="text-center text-white">Total Amount: ₹<span id="totalAmount">{totalAmount}</span></h3>
+        <h3 className="text-center text-white">Total Amount: ₹<span id="totalAmount">{totalAmount}</span></h3>
       </div>
-      <center><button class="rounded payment my-3 h6">Proceed to Payment</button></center>
+      <center><button className="rounded payment my-3 h6">Proceed to Payment</button></center>
       <Footer />
     </div>
   );
